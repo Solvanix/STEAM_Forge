8c1127284424c13e9f0ff2c7774546361401ebd7
Ali-Khateb-1 <ali.khateb@gmail.com>
2025-06-12 23:13:21 +0300

    مزامنة تحديثات STEAM-Forge

---
+++ utils_1.py
@@ -0,0 +1,31 @@
+import os
+import json
+import shutil
+import yaml
+import h5py
+import dill
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+import PIL
+import cv2
+import imageio
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import sklearn
+
+import segno
+import qrcode
+import subprocess
+import tqdm
+import ipywidgets
+import ruamel.yaml
+ 
